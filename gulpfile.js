ed8d865b66140e45d9788b1ccae9d7eed44fdf1c
t-kimura <tosikimu@gmail.com>
2016-12-21 01:21:59 +0900

    add gulp task project init

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,16 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const config = require('./webpack.config.js');
 
+const runSequence = require('run-sequence');
+
 const cordova = require('gulp-cordova');
+
+gulp.task('init', () => {
+  return runSequence(
+    'webpack',
+    'cordova:init'
+  );
+});
 
 gulp.task('webpack', () => {
   return gulp
