6e69e399868cd730bcb9cbca8ad0b6e5662c10ef
t-kimura <tosikimu@gmail.com>
2016-12-21 01:07:46 +0900

    gulp task add cordova:init

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const config = require('./webpack.config.js');
 
+const cordova = require('gulp-cordova');
+
 gulp.task('webpack', () => {
   return gulp
     .src(config.entry)
@@ -12,4 +14,9 @@ gulp.task('webpack', () => {
     .pipe(
         gulp.dest(config.output.path)
     );
+});
+
+gulp.task('cordova:init', () => {
+  gulp.src('./package.json')
+    .pipe(cordova());
 });
